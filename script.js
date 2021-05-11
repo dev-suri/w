3038a5ee521743c7677c7e45189aaf50b54aeb22
dev-suri <47437894+dev-suri@users.noreply.github.com>
2021-05-11 16:49:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 window.onload = displayClock();
 function displayClock(){
-  document.getElementById("timer").innerHTML = new Date().toLocaleTimeString().toLowerCase();;
-  document.getElementById("date").innerHTML = new Date().toLocaleDateString().toLowerCase();;
+  var d = new Date();
+  document.getElementById("date").innerHTML = d.getMonth() + "/" d.getDate() + "/" + d.getFullYear();
+  document.getElementById("timer").innerHTML = d.toLocaleTimeString();
   setTimeout(displayClock, 1000);
 }
 
